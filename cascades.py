e985ccd43f42f54a6828edeb5ab486be7535f548
Srinivas Kaza <srinivas@kaza.io>
2017-01-20 17:59:37 -0500

    Now searches for optimal value

--- cascades.py
+++ cascades.py
@@ -10,7 +10,14 @@ g = GlobalParams(96, 4.8)
 
 
 class TollElement(object):
-    pass
+
+    def compute_output_num(self):
+        raise NotImplementedError("Haven't yet implemented this cascade's " +
+                                  "compute_output_num() function")
+
+    def compute_input_num(self):
+        raise NotImplementedError("Haven't yet implemented this cascade's " +
+                                  "compute_input_num() function")
 
 
 class Lane(TollElement):
@@ -21,6 +28,15 @@ class Lane(TollElement):
     def compute_dist(self):
         return TV(self.params.h, self.params.v)
 
+    def compute_output_num(self):
+        return 1
+
+    def compute_input_num(self):
+        return 1
+
+    def __repr__(self):
+        return "Lane {{\n\tTV: {}}}".format(self.compute_dist())
+
 
 class Cascade(TollElement):
 
@@ -28,17 +44,12 @@ class Cascade(TollElement):
         self.elements = elements
         self.params = params
 
+    def get_lanes(self):
+        return [Lane(LaneParams(tv.h, tv.v)) for tv in self.compute_dist()]
+
     def compute_dist(self):
         raise NotImplementedError("Haven't yet implemented this cascade's " +
                                   "compute_dist() function")
-
-    def compute_output_num(self):
-        raise NotImplementedError("Haven't yet implemented this cascade's " +
-                                  "compute_output_num() function")
-
-    def compute_input_num(self):
-        raise NotImplementedError("Haven't yet implemented this cascade's " +
-                                  "compute_input_num() function")
 
     def compute_ratio(self):
         inputs = sum(a.compute_dist().h for a in self.elements)
@@ -68,6 +79,10 @@ class LCascade(Cascade):
     def compute_output_num(self):
         return 1
 
+    def __repr__(self):
+        return "LCascade {{\n\t{}\n\t{}\n\tTV: {}}}".format(
+            self.elements[0], self.elements[1], self.compute_dist())
+
 
 class RCascade(Cascade):
 
@@ -90,6 +105,10 @@ class RCascade(Cascade):
 
     def compute_output_num(self):
         return 1
+
+    def __repr__(self):
+        return "RCascade {{\n\t{}\n\t{}\n\tTV: {}}}".format(
+            self.elements[1], self.elements[0], self.compute_dist())
 
 
 class TriCascade(Cascade):
@@ -118,6 +137,11 @@ class TriCascade(Cascade):
     def compute_output_num(self):
         return 1
 
+    def __repr__(self):
+        return "TriCascade {{\n\t{}\n\t{}\n{}\n\tTV: {}}}".format(
+            self.elements[0], self.elements[1], self.elements[2],
+            self.compute_dist())
+
 
 class DivCascade(Cascade):
 
@@ -138,8 +162,8 @@ class DivCascade(Cascade):
 
         # Fix speed
         return (
-            TV(a.h + H(alpha(a) - alpha(c)) * alpha(a) * b.h, b.h),
-            TV(c.h + H(alpha(c) - alpha(a)) * alpha(c) * b.h, b.h)
+            TV(a.h + H(alpha(a) - alpha(c)) * alpha(a) * b.h, b.v),
+            TV(c.h + H(alpha(c) - alpha(a)) * alpha(c) * b.h, b.v)
         )
 
     def compute_input_num(self):
@@ -148,10 +172,59 @@ class DivCascade(Cascade):
     def compute_output_num(self):
         return 2
 
+    def __repr__(self):
+        return "DivCascade {{\n\t{}\n\t{}\n\t{}\n\tTV: {}}}".format(
+            self.elements[0], self.elements[1], self.elements[2],
+            self.compute_dist())
+
+
+def generate(input_lanes, target_number, structures=[]):
+    if len(input_lanes) == target_number:
+        return [(input_lanes, structures)]
+    if len(input_lanes) < target_number:
+        return []
+    possibilities = []
+    for i in range(len(input_lanes) - 1):
+        l_cas = LCascade(input_lanes[i], input_lanes[i + 1], None)
+        r_cas = RCascade(input_lanes[i], input_lanes[i + 1], None)
+        possibilities.extend(
+            generate(input_lanes[:i] +
+                     l_cas.get_lanes() +
+                     input_lanes[(i + 2):], target_number, structures + [l_cas]))
+        possibilities.extend(
+            generate(input_lanes[:i] +
+                     r_cas.get_lanes() +
+                     input_lanes[(i + 2):], target_number, structures + [r_cas]))
+        if (i != len(input_lanes) - 2):
+            tri_cas = TriCascade(input_lanes[i],
+                                 input_lanes[i + 1],
+                                 input_lanes[i + 2], None)
+            div_cas = DivCascade(input_lanes[i],
+                                 input_lanes[i + 1],
+                                 input_lanes[i + 2], None)
+            possibilities.extend(
+                generate(input_lanes[:i] +
+                         tri_cas.get_lanes() +
+                         input_lanes[(i + 3):],
+                         target_number,
+                         structures + [tri_cas]))
+            possibilities.extend(
+                generate(input_lanes[:i] +
+                         div_cas.get_lanes() +
+                         input_lanes[(i + 3):],
+                         target_number,
+                         structures + [div_cas]))
+    return possibilities
+
 if __name__ == '__main__':
-    a = DivCascade(
-        Lane(LaneParams(0.09, 29)),
-        Lane(LaneParams(0.09, 29)),
-        Lane(LaneParams(0.09, 29)), None)
-    print(a.compute_dist())
-    print(a.compute_ratio())
+    a = [Lane(LaneParams(0.2, 29)) for a in range(8)]
+    best_score = 0
+    best = None
+    input_h = sum(lane.compute_dist().h for lane in a)
+    for config in generate(a, 3):
+        output_h = sum(lane.compute_dist().h for lane in config[0])
+        score = output_h / input_h
+        if best_score < score:
+            best_score = score
+            best = config
+    print("{}:\n{}\n\n".format(best[1], best_score))
